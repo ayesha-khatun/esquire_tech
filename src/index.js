423c880c9a5493d6b9f9ebf2bd17751cdda530e6
ayesha2 <ayesha.k@esquire.com.bd>
2022-05-24 16:10:13 -0700

    push

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import 'font-awesome/css/font-awesome.min.css';
 // import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter, BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
